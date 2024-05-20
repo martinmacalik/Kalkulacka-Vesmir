988ac5e78419d380c0a69f9a8b2ecbb27f06ca74
martinmacalik <119074659+martinmacalik@users.noreply.github.com>
2024-05-20 14:35:17 +0200

    Add files via upload

---
+++ planety.py
@@ -0,0 +1,437 @@
+print("Vítej v té nejužitečnější kalkulačce vůbec!")
+print("Do jakého vesmírného objektu chceš nacpat který jiný vesmírný objekt?")
+print("Na výběr máš planety sluneční soustavy, Slunce, Měsíc a největší hvězdu VY Canis Major")
+print("Vyber si první vesmírný objekt")
+a = input()
+print("Vyber si druhý vesmírný objekt")
+b = input()
+
+a = a.lower()
+b = b.lower()
+
+merkur = 60083000000
+venuse = 928430000000
+zeme = 1083210000000
+mars = 163100000000
+jupiter = 1431300000000000
+saturn = 827130000000000
+uran = 68330000000000
+neptun = 62530000000000
+slunce = 1480000000000000000
+mesic = 21958000000
+vycm = 7552559551492160320037715968
+
+dik = f"Děkuji za použití kalkulačky. :D"
+
+if (a == "merkur"):
+    if (b == "merkur"):
+        vysledek = merkur / merkur
+        print(vysledek)
+    elif (b == "venuše"):
+        vysledek = merkur / venuse
+        print(vysledek)
+    elif (b == "země"):
+        vysledek = merkur / zeme
+        print(vysledek)
+    elif (b == "mars"):
+        vysledek = merkur / mars
+        print(vysledek)
+    elif (b == "jupiter"):
+        vysledek = merkur / jupiter
+        print(vysledek)
+    elif (b == "saturn"):
+        vysledek = merkur / saturn
+        print(vysledek)
+    elif (b == "uran"):
+        vysledek = merkur / uran
+        print(vysledek)
+    elif (b == "neptun"):
+        vysledek =  merkur / neptun
+        print(vysledek)
+    elif (b == "měsíc"):
+        vysledek = merkur / mesic
+        print(vysledek)
+    elif (b == "slunce"):
+        vysledek = merkur / slunce
+        print(vysledek)
+    elif (b == "vy canis major"):
+        vysledek = merkur / vycm
+        print(vysledek)
+    else :
+        print("špatné zadání")
+
+elif (a == "venuše"):
+    if (b == "merkur"):
+        vysledek = venuse / merkur
+        print(vysledek)
+    elif (b == "venuše"):
+        vysledek = venuse / venuse
+        print(vysledek)
+    elif (b == "země"):
+        vysledek = venuse / zeme
+        print(vysledek)
+    elif (b == "mars"):
+        vysledek = venuse / mars
+        print(vysledek)
+    elif (b == "jupiter"):
+        vysledek = venuse / jupiter
+        print(vysledek)
+    elif (b == "saturn"):
+        vysledek = venuse / saturn
+        print(vysledek)
+    elif (b == "uran"):
+        vysledek = venuse / uran
+        print(vysledek)
+    elif (b == "neptun"):
+        vysledek = venuse / neptun
+        print(vysledek)
+    elif (b == "měsíc"):
+        vysledek = venuse / mesic
+        print(vysledek)
+    elif (b == "slunce"):
+        vysledek = venuse / slunce
+        print(vysledek)
+    elif (b == "vy canis major"):
+        vysledek = venuse / vycm
+        print(vysledek)
+    else :
+        print("špatné zadání")
+
+elif (a == "země"):
+    if (b == "merkur"):
+        vysledek = zeme / merkur
+        print(vysledek)
+    elif (b == "venuše"):
+        vysledek = zeme / venuse
+        print(vysledek)
+    elif (b == "země"):
+        vysledek = zeme / zeme
+        print(vysledek)
+    elif (b == "mars"):
+        vysledek = zeme / mars
+        print(vysledek)
+    elif (b == "jupiter"):
+        vysledek = zeme / jupiter
+        print(vysledek)
+    elif (b == "saturn"):
+        vysledek = zeme / saturn
+        print(vysledek)
+    elif (b == "uran"):
+        vysledek = zeme / uran
+        print(vysledek)
+    elif (b == "neptun"):
+        vysledek = zeme / neptun
+        print(vysledek)
+    elif (b == "měsíc"):
+        vysledek = zeme / mesic
+        print(vysledek)
+    elif (b == "slunce"):
+        vysledek = zeme / slunce
+        print(vysledek)
+    elif (b == "vy canis major"):
+        vysledek = zeme / vycm
+        print(vysledek)
+    else :
+        print("špatné zadání")
+
+elif (a == "mars"):
+    if (b == "merkur"):
+        vysledek = mars / merkur
+        print(vysledek)
+    elif (b == "venuše"):
+        vysledek = mars / venuse
+        print(vysledek)
+    elif (b == "země"):
+        vysledek = mars / zeme
+        print(vysledek)
+    elif (b == "mars"):
+        vysledek = mars / mars
+        print(vysledek)
+    elif (b == "jupiter"):
+        vysledek = mars / jupiter
+        print(vysledek)
+    elif (b == "saturn"):
+        vysledek = mars / saturn
+        print(vysledek)
+    elif (b == "uran"):
+        vysledek = mars / uran
+        print(vysledek)
+    elif (b == "neptun"):
+        vysledek = mars / neptun
+        print(vysledek)
+    elif (b == "měsíc"):
+        vysledek = mars / mesic
+        print(vysledek)
+    elif (b == "slunce"):
+        vysledek = mars / slunce
+        print(vysledek)
+    elif (b == "vy canis major"):
+        vysledek = mars / vycm
+        print(vysledek)
+    else :
+        print("špatné zadání")
+
+elif (a == "jupiter"):
+    if (b == "merkur"):
+        vysledek = jupiter / merkur
+        print(vysledek)
+    elif (b == "venuše"):
+        vysledek = jupiter / venuse
+        print(vysledek)
+    elif (b == "země"):
+        vysledek = jupiter / zeme
+        print(vysledek)
+    elif (b == "mars"):
+        vysledek = jupiter / mars
+        print(vysledek)
+    elif (b == "jupiter"):
+        vysledek = jupiter / jupiter
+        print(vysledek)
+    elif (b == "saturn"):
+        vysledek = jupiter / saturn
+        print(vysledek)
+    elif (b == "uran"):
+        vysledek = jupiter / uran
+        print(vysledek)
+    elif (b == "neptun"):
+        vysledek = jupiter / neptun
+        print(vysledek)
+    elif (b == "měsíc"):
+        vysledek = jupiter / mesic
+        print(vysledek)
+    elif (b == "slunce"):
+        vysledek = jupiter / slunce
+        print(vysledek)
+    elif (b == "vy canis major"):
+        vysledek =  jupiter / vycm
+        print(vysledek)
+    else :
+        print("špatné zadání")
+
+elif (a == "saturn"):
+    if (b == "merkur"):
+        vysledek = saturn / merkur
+        print(vysledek)
+    elif (b == "venuše"):
+        vysledek = saturn / venuse
+        print(vysledek)
+    elif (b == "země"):
+        vysledek = saturn / zeme
+        print(vysledek)
+    elif (b == "mars"):
+        vysledek = saturn / mars
+        print(vysledek)
+    elif (b == "jupiter"):
+        vysledek = saturn / jupiter
+        print(vysledek)
+    elif (b == "saturn"):
+        vysledek = saturn / saturn
+        print(vysledek)
+    elif (b == "uran"):
+        vysledek = saturn / uran
+        print(vysledek)
+    elif (b == "neptun"):
+        vysledek = saturn / neptun
+        print(vysledek)
+    elif (b == "měsíc"):
+        vysledek = saturn / mesic
+        print(vysledek)
+    elif (b == "slunce"):
+        vysledek = saturn / slunce
+        print(vysledek)
+    elif (b == "vy canis major"):
+        vysledek = saturn / vycm
+        print(vysledek)
+    else :
+        print("špatné zadání")
+
+elif (a == "uran"):
+    if (b == "merkur"):
+        vysledek = uran / merkur
+        print(vysledek)
+    elif (b == "venuše"):
+        vysledek = uran / venuse
+        print(vysledek)
+    elif (b == "země"):
+        vysledek = uran / zeme
+        print(vysledek)
+    elif (b == "mars"):
+        vysledek = uran / mars
+        print(vysledek)
+    elif (b == "jupiter"):
+        vysledek = uran / jupiter
+        print(vysledek)
+    elif (b == "saturn"):
+        vysledek = uran / saturn
+        print(vysledek)
+    elif (b == "uran"):
+        vysledek = uran / uran
+        print(vysledek)
+    elif (b == "neptun"):
+        vysledek = uran / neptun
+        print(vysledek)
+    elif (b == "měsíc"):
+        vysledek = uran / mesic
+        print(vysledek)
+    elif (b == "slunce"):
+        vysledek = uran / slunce
+        print(vysledek)
+    elif (b == "vy canis major"):
+        vysledek = uran / vycm
+        print(vysledek)
+    else :
+        print("špatné zadání")
+
+elif (a == "neptun"):
+    if (b == "merkur"):
+        vysledek = neptun / merkur
+        print(vysledek)
+    elif (b == "venuše"):
+        vysledek = neptun / venuse
+        print(vysledek)
+    elif (b == "země"):
+        vysledek = neptun / zeme
+        print(vysledek)
+    elif (b == "mars"):
+        vysledek = neptun / mars
+        print(vysledek)
+    elif (b == "jupiter"):
+        vysledek = neptun / jupiter
+        print(vysledek)
+    elif (b == "saturn"):
+        vysledek = neptun / saturn
+        print(vysledek)
+    elif (b == "uran"):
+        vysledek = neptun / uran
+        print(vysledek)
+    elif (b == "neptun"):
+        vysledek = neptun / neptun
+        print(vysledek)
+    elif (b == "měsíc"):
+        vysledek = neptun / mesic
+        print(vysledek)
+    elif (b == "slunce"):
+        vysledek = neptun / slunce
+        print(vysledek)
+    elif (b == "vy canis major"):
+        vysledek = neptun / vycm
+        print(vysledek)
+    else :
+        print("špatné zadání")
+
+elif(a == "slunce"):
+    if (b == "merkur"):
+        vysledek = slunce / merkur
+        print(vysledek)
+    elif (b == "venuše"):
+        vysledek = slunce / venuse
+        print(vysledek)
+    elif (b == "země"):
+        vysledek = slunce / zeme
+        print(vysledek)
+    elif (b == "mars"):
+        vysledek = slunce / mars
+        print(vysledek)
+    elif (b == "jupiter"):
+        vysledek = slunce / jupiter
+        print(vysledek)
+    elif (b == "saturn"):
+        vysledek = slunce / saturn
+        print(vysledek)
+    elif (b == "uran"):
+        vysledek = slunce / uran
+        print(vysledek)
+    elif (b == "neptun"):
+        vysledek = slunce / neptun
+        print(vysledek)
+    elif (b == "měsíc"):
+        vysledek = slunce / mesic
+        print(vysledek)
+    elif (b == "slunce"):
+        vysledek = slunce / slunce
+        print(vysledek)
+    elif (b == "vy canis major"):
+        vysledek = slunce / vycm
+        print(vysledek)
+    else :
+        print("špatné zadání")
+
+elif (a == "měsíc"):
+    if (b == "merkur"):
+        vysledek = mesic / merkur
+        print(vysledek)
+    elif (b == "venuše"):
+        vysledek = mesic / venuse
+        print(vysledek)
+    elif (b == "země"):
+        vysledek = mesic / zeme
+        print(vysledek)
+    elif (b == "mars"):
+        vysledek = mesic / mars
+        print(vysledek)
+    elif (b == "jupiter"):
+        vysledek = mesic / jupiter
+        print(vysledek)
+    elif (b == "saturn"):
+        vysledek = mesic / saturn
+        print(vysledek)
+    elif (b == "uran"):
+        vysledek = mesic / uran
+        print(vysledek)
+    elif (b == "neptun"):
+        vysledek = mesic / neptun
+        print(vysledek)
+    elif (b == "měsíc"):
+        vysledek = mesic / mesic
+        print(vysledek)
+    elif (b == "slunce"):
+        vysledek = mesic / slunce
+        print(vysledek)
+    elif (b == "vy canis major"):
+        vysledek = mesic / vycm
+        print(vysledek)
+    else :
+        print("špatné zadání")
+
+elif (a == "vy canis major"):
+    if (b == "merkur"):
+        vysledek = vycm / merkur
+        print(vysledek)
+    elif (b == "venuše"):
+        vysledek =  vycm / venuse
+        print(vysledek)
+    elif (b == "země"):
+        vysledek = vycm / zeme
+        print(vysledek)
+    elif (b == "mars"):
+        vysledek = vycm / mars
+        print(vysledek)
+    elif (b == "jupiter"):
+        vysledek = vycm / jupiter
+        print(vysledek)
+    elif (b == "saturn"):
+        vysledek = vycm / saturn
+        print(vysledek)
+    elif (b == "uran"):
+        vysledek = vycm / uran
+        print(vysledek)
+    elif (b == "neptun"):
+        vysledek = vycm / neptun
+        print(vysledek)
+    elif (b == "měsíc"):
+        vysledek = vycm / mesic
+        print(vysledek)
+    elif (b == "slunce"):
+        vysledek = vycm / slunce
+        print(vysledek)
+    elif (b == "vy canis major"):
+        vysledek = vycm / vycm
+        print(vysledek)
+    else :
+        print("špatné zadání")
+else :
+    print("Špatné zadání, zkontroluj si že jsi napsal vstup bez chyby.")
+
+print("Pro upřesnění ti vyšlo číslo. Kolikrát se vleze", b, "do", a, ".")
+print("Děkuji za použití mojí kalkulačky.")
+ukoncit  = input("Ukončíš stisknutím libovolné klávesy.")
